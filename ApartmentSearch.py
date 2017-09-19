97c51b4b66f5975e674d45cae8fc7b18ff8b2acc
JRHutson <hutson.regan@gmail.com>
2017-09-18 20:38:09 -0700

    Hard Coded result for testing

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -8,12 +8,14 @@ cl_h = CraigslistHousing(site='losangeles', area='sgv', category='apa',
 #for result in cl_h.get_results(sort_by='newest', geotagged=True):
 #    print (result['geotag'])
 
+result = {'id': '6301716374', 'name': 'Beautiful and spacious 1bd in central area', 'url': 'https://losangeles.craigslist.org/sgv/apa/d/beautiful-and-spacious-1bd-in/6301716374.html', 'datetime': '2017-09-11 10:28', 'price': '$1795', 'where': '85 N Holliston Ave. Pasadena', 'has_image': True, 'has_map': True, 'geotag': (34.147648, -118.123046), 'bedrooms': '1', 'area': None}
+
 idx = index.Index()
 
-idx.insert(1, (34.113318, -118.071612, 34.112352, -118.073473)
+idx.insert(1, (34.113318, -118.071612, 34.112352, -118.073473))
 
-for result in cl_h.get_results(sort_by='newest', geotagged=True):
-    test = list(idx.intersection(result['geotag']))
-        if 1 in test:
-            print(result['name'])
+print(result['geotag'])
+print(location)
+
+
 
