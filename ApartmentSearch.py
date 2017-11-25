88da8aed485442b270f9c3a5e1b3c671c645faba
JRHutson <hutson.regan@gmail.com>
2017-11-25 13:59:42 -0800

    Getting only results with a Geotag
    Using Try/Except to only continue code when a valid Geotag is present.

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -5,20 +5,25 @@ from rtree import index
 cl_h = CraigslistHousing(site='losangeles', area='sgv', category='apa',
                          filters={'max_price': 1300, 'zip_code': 91106, 'search_distance': 10, 'max_bedrooms': 1})
 
-#for result in cl_h.get_results(sort_by='newest', geotagged=True):
-#    print (result['geotag'])
+for result in cl_h.get_results(sort_by='newest', geotagged=True):
+    try:
+        location = result['geotag']
+        latitude = location[0]
+        print(latitude)
+        longitude = location[1]
+        print(longitude)
+    except:
+        continue
+    
+#result = {'id': '6301716374', 'name': 'Beautiful and spacious 1bd in central area', 'url': 'https://losangeles.craigslist.org/sgv/apa/d/beautiful-and-spacious-1bd-in/6301716374.html', 'datetime': '2017-09-11 10:28', 'price': '$1795', 'where': '85 N Holliston Ave. Pasadena', 'has_image': True, 'has_map': True, 'geotag': (34.147648, -118.123046), 'bedrooms': '1', 'area': None}
 
-result = {'id': '6301716374', 'name': 'Beautiful and spacious 1bd in central area', 'url': 'https://losangeles.craigslist.org/sgv/apa/d/beautiful-and-spacious-1bd-in/6301716374.html', 'datetime': '2017-09-11 10:28', 'price': '$1795', 'where': '85 N Holliston Ave. Pasadena', 'has_image': True, 'has_map': True, 'geotag': (34.147648, -118.123046), 'bedrooms': '1', 'area': None}
-
-idx = index.Index()
-
-idx.insert(1, (34.112352, -118.073473, 34.113318, -118.071612))
-
-
-print(list(idx.intersection((34.112542, -118.072567, 34.112542, -118.072567))))
-
-print(result['geotag'])
-
-
-
-
+##idx = index.Index()
+##
+##idx.insert(1, (34.112352, -118.073473, 34.113318, -118.071612))
+##
+##
+##print(list(idx.intersection((34.112542, -118.072567, 34.112542, -118.072567))))
+##
+##print(result['geotag'])
+##
+##
