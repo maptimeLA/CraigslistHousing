eee59f023b619cf562d6b206a9121726dd05f192
JRHutson <hutson.regan@gmail.com>
2017-11-25 14:37:36 -0800

    Successful index query.
    Corrected order of Lat/Long input into Index and added test to see if
    query for each result gives a True result.

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -2,6 +2,12 @@
 from craigslist import CraigslistHousing
 from rtree import index
 
+#Creating Index
+idx = index.Index()
+
+#Adding Giant Box to index
+idx.insert(1, (-118.156726, 34.083989, -117.950682, 34.211280))
+
 cl_h = CraigslistHousing(site='losangeles', area='sgv', category='apa',
                          filters={'max_price': 1300, 'zip_code': 91106, 'search_distance': 10, 'max_bedrooms': 1})
 
@@ -9,21 +15,16 @@ for result in cl_h.get_results(sort_by='newest', geotagged=True):
     try:
         location = result['geotag']
         latitude = location[0]
-        print(latitude)
+        #print(latitude)
         longitude = location[1]
-        print(longitude)
+        #print(longitude)
     except:
         continue
-    
-#result = {'id': '6301716374', 'name': 'Beautiful and spacious 1bd in central area', 'url': 'https://losangeles.craigslist.org/sgv/apa/d/beautiful-and-spacious-1bd-in/6301716374.html', 'datetime': '2017-09-11 10:28', 'price': '$1795', 'where': '85 N Holliston Ave. Pasadena', 'has_image': True, 'has_map': True, 'geotag': (34.147648, -118.123046), 'bedrooms': '1', 'area': None}
 
-##idx = index.Index()
-##
-##idx.insert(1, (34.112352, -118.073473, 34.113318, -118.071612))
-##
-##
-##print(list(idx.intersection((34.112542, -118.072567, 34.112542, -118.072567))))
-##
-##print(result['geotag'])
-##
-##
+    query = list(idx.intersection((longitude, latitude, longitude, latitude)))
+    print(query)
+    if not query:
+        print("Outside Search Area")
+    else:
+        print(result['geotag'])
+    
