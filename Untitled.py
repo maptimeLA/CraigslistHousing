102d826a4fc87af5363145fb3490bac1bd451338
JRHutson <hutson.regan@gmail.com>
2017-11-25 16:23:00 -0800

    File Cleanup

--- Untitled.py
+++
@@ -1,8 +0,0 @@
-from craigslist import CraigslistHousing
-
-clh = CraigslistHousing(site='losangeles', area='sgv', category='apa', filters={'max_price' : 1350})
-
-for result in clh:
-    print result
-
-    
