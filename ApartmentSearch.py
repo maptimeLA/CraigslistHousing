cff171eedea78d072fcbf74d952f8cbde42f3d3f
JRHutson <hutson.regan@gmail.com>
2017-11-26 11:04:13 -0800

    Added function to Calculate distance.
    Added function to calculate distance between Geographic Coords. Using
    to output distance to nearest station.

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -2,11 +2,30 @@
 ##https://www.dataquest.io/blog/apartment-finding-slackbot/
 ##https://opensource.com/article/17/10/set-postgres-database-your-raspberry-pi
 ##https://suhas.org/sqlalchemy-tutorial/
+##https://stackoverflow.com/questions/15736995/how-can-i-quickly-estimate-the-distance-between-two-latitude-longitude-points
 
 
 from craigslist import CraigslistHousing
 from rtree import index
 import json
+from math import radians, cos, sin, asin, sqrt
+
+def coord_distance(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    # Radius of earth in kilometers is 6371
+    km = 6371* c
+    mi = km * 0.621371
+    return mi
 
 #Creating Index
 idx = index.Index()
@@ -49,6 +68,9 @@ for result in cl_h.get_results(sort_by='newest', geotagged=True):
         #print("Outside Search Area")
     else:
         print(result['geotag'])
+        stationLat = data['features'][query[0]]['properties']['LAT']
+        stationLong = data['features'][query[0]]['properties']['LONG']
+        dist = coord_distance(stationLong, stationLat, result['geotag'][1], result['geotag'][0])
         print(result['url'])
-        print(data['features'][query[0]]['properties']['STATION'])
+        print('Only ' + str(round(dist, 2)) + 'mi from ' + data['features'][query[0]]['properties']['STATION'])
     
