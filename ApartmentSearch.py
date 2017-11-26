05209b0eda426c6b7c61eb08ce38ab7fbe451390
JRHutson <hutson.regan@gmail.com>
2017-11-26 10:13:01 -0800

    Updated Search Criteria.

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -31,7 +31,7 @@ print("Index Complete")
 
 
 cl_h = CraigslistHousing(site='losangeles', area='sgv', category='apa',
-                         filters={'max_price': 1500, 'max_bedrooms': 1})
+                         filters={'max_price': 1500, 'min_price': 1000, 'min_bedrooms':1, 'max_bedrooms': 1})
 
 for result in cl_h.get_results(sort_by='newest', geotagged=True):
     try:
