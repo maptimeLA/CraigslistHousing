cae938340af4515aef3b0dec0ad3fb41078f9c56
JRHutson <hutson.regan@gmail.com>
2017-11-26 11:42:04 -0800

    Added Slack Integration

--- ApartmentSearch.py
+++ ApartmentSearch.py
@@ -9,6 +9,10 @@ from craigslist import CraigslistHousing
 from rtree import index
 import json
 from math import radians, cos, sin, asin, sqrt
+from slackclient import SlackClient
+
+SLACK_TOKEN = "InsertSlackToken"
+SLACK_CHANNEL = "#craigslist"
 
 def coord_distance(lon1, lat1, lon2, lat2):
     """
@@ -52,6 +56,8 @@ print("Index Complete")
 cl_h = CraigslistHousing(site='losangeles', area='sgv', category='apa',
                          filters={'max_price': 1500, 'min_price': 1000, 'min_bedrooms':1, 'max_bedrooms': 1})
 
+sc = SlackClient(SLACK_TOKEN)
+
 for result in cl_h.get_results(sort_by='newest', geotagged=True):
     try:
         location = result['geotag']
@@ -73,4 +79,9 @@ for result in cl_h.get_results(sort_by='newest', geotagged=True):
         dist = coord_distance(stationLong, stationLat, result['geotag'][1], result['geotag'][0])
         print(result['url'])
         print('Only ' + str(round(dist, 2)) + 'mi from ' + data['features'][query[0]]['properties']['STATION'])
+        desc = "{0} | {1} mi from {2} | {3} | <{4}>".format(result["price"], str(round(dist,2)), data['features'][query[0]]['properties']['STATION'], result["name"], result["url"])
+        sc.api_call(
+        "chat.postMessage", channel=SLACK_CHANNEL, text=desc,
+        username='pybot', icon_emoji=':robot_face:'
+        )
     
